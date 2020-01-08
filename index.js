ae7240fe16df10fe5051dc1cfee6884813653ac5
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-08 23:40:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  let data = {
+    name: userName,
+    email: userEmail
+  }
+    let configObj = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+
+    fetch('http://localhost:3000/users', configObj)
+     .then(function(response){
+       return response.json()
+     })
+     .then(function(object){
+       document.body.innerHTML = console.log(object["id"])
+     })
+     .catch(function(error) {
+        document.body.innerHTML = error.message
+     })
+
+}
